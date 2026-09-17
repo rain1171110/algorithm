3a46048e218f47f51d4e84b0d6b75b807db1bbd5
岡崎陽平 <rain1171110>
2026-09-17 17:09:25 +0900

    学習ログ

--- mini.js
+++ mini.js
@@ -36,7 +36,6 @@
 const fs = require("fs");
 const lines = fs.readFileSync(0, "utf-8").trim().split(/\n+/);
 
-
 let variable1 = 0;
 let variable2 = 0;
 
@@ -51,8 +50,9 @@ for (let i = 1; i <= n; i++) {
       variable2 = Number(command[2]);
     }
   } else if (command[0] === "ADD") {
-    
+    variable2 = variable1 + Number(command[1]);
   } else if (command[0] === "SUB") {
+    variable2 = variable1 - Number(command[1]);
   }
 }
 
